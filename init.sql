e9c80b83d4c358f1461b107d874bcc7858a70611
haranton2003 <haranton2003@gmai.com>
2025-10-07 18:02:16 +0300

    1

--- init.sql
+++ init.sql
@@ -1,2 +1,6 @@
-CREATE DATABASE db;
-CREATE DATABASE db_test;
+-- Создаем базу данных если не существует
+SELECT 'CREATE DATABASE db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db')\gexec
+
+SELECT 'CREATE DATABASE db_test'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db_test')\gexec
